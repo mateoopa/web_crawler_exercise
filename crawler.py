95757966a596f35be77ad1f6612664d2b7e1953e
THINKPAD\Mateo <mposadaagudelosn@gmail.com>
2023-02-28 13:35:38 -0500

    upload

---
+++ crawler.py
@@ -0,0 +1,66 @@
+from selenium.webdriver.common.by import By
+import module
+import json
+import validators
+import traceback
+
+
+class WebCrawler:
+
+    def __init__(self):
+        self.parameters = module.GetParam()
+        self.bot = module.Selenium()
+        self.results = None
+
+    def get_images(self):
+        self.bot.navigate(self.parameters.start_url, True)
+        img_xpath = '//descendant::img[1]'
+        links_xpath = '//a[@href and not(@disabled)]'
+
+        links = self.bot.fe(multiple=True, by=By.XPATH, elements=links_xpath, stop=True)
+        all_links = []
+        results = []
+
+        for counter, link in enumerate(links, 0):
+            try:
+                if counter > self.parameters.depth:
+                    break
+                else:
+                    current_link = link.get_attribute('href')
+                    if current_link not in all_links:
+                        all_links.append(current_link)
+                    else:
+                        self.parameters.depth = self.parameters.depth + 1
+                        continue
+            except (Exception,):
+                print(f'{traceback.format_exc()} {locals()}')
+                continue
+
+        for counter, link in enumerate(all_links, 0):
+
+            if validators.url(link):
+                self.bot.navigate(link, True)
+                images = self.bot.fe(multiple=True, by=By.XPATH, elements=img_xpath, stop=True)
+
+                for image in images:
+                    try:
+                        my_dict = {'imageUrl': image.get_attribute('src'),
+                                   'sourceUrl': self.bot.driver.current_url,
+                                   'depth': counter}
+                        results.append(my_dict)
+                    except (Exception,):
+                        continue
+        self.bot.quit_driver()
+        self.results = {"results": [results]}
+
+    def save_json(self):
+        json_object = json.dumps(self.results, indent=4)
+
+        with open("results.json", "w") as outfile:
+            outfile.write(json_object)
+
+
+crawler = WebCrawler()
+crawler.get_images()
+crawler.save_json()
+
---
+++ module.py
@@ -0,0 +1,97 @@
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import UnexpectedAlertPresentException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium import webdriver
+import traceback
+import sys
+
+
+class Selenium:
+
+    def __init__(self):
+        self.time = 10
+
+        try:
+            options = Options()
+            options.add_argument('--headless')
+            options.add_argument("--ignore-certificate-error")
+            options.add_argument("--ignore-ssl-errors")
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            self.driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+        except (Exception,):
+            print(f'{traceback.format_exc()} {locals()}')
+            self.quit_driver()
+
+    def navigate(self, url: str, stop: bool):
+        try:
+            self.driver.get(url)
+        except (Exception,):
+            print(f'{traceback.format_exc()} {locals()}')
+            self.quit_driver()
+
+    def wfe(self, by: By, element: str, stop: bool, element_name: str = None):
+        """
+
+        Wait for element
+
+        """
+        try:
+            WebDriverWait(self.driver, self.time).until(ec.presence_of_element_located((by, element)))
+            return True
+        except (TimeoutException, UnexpectedAlertPresentException) as e:
+            if stop:
+                print(f'{traceback.format_exc()} {locals()}')
+                self.quit_driver()
+            else:
+                return False
+        except (Exception,):
+            if not stop:
+                return False
+            print(f'{traceback.format_exc()} {locals()}')
+            self.quit_driver()
+
+    def fe(self, multiple: bool, by: By, elements: str, stop: bool):
+
+        try:
+            Selenium.wfe(self, by, elements, stop)
+            if multiple:
+                return self.driver.find_elements(by, elements)
+            else:
+                return self.driver.find_element(by, elements)
+
+        except (TimeoutException, NoSuchElementException) as e:
+            if stop:
+                print(f'{traceback.format_exc()} {locals()}')
+                self.quit_driver()
+            else:
+                return False
+        except (Exception,):
+            if not stop:
+                return False
+            print(f'{traceback.format_exc()} {locals()}')
+            self.quit_driver()
+
+    def quit_driver(self):
+        try:
+            if self.driver:
+                self.driver.quit()
+        except (Exception,):
+            print(f'{traceback.format_exc()} {locals()}')
+
+
+class GetParam:
+
+    def __init__(self):
+        if len(sys.argv) == 3:
+            self.start_url = str(sys.argv[1])
+            self.depth = int(sys.argv[2])
+            # self.start_url = 'https://pimeyes.com/en'
+            # self.depth = 3
+        else:
+            print('Please pass parameters in this order <start_url: string> <depth: number>')
+            sys.exit(0)
